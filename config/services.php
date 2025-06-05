3483b3e0337a8e05db3ae8dd02dae826b58ba6e2
Brave-gato <webrepoleved@google.com>
2025-06-05 19:06:49 +0200

    ajout askController, création de Services

--- config/services.php
+++ config/services.php
@@ -35,4 +35,8 @@ return [
         ],
     ],
 
+    'openrouter' => [
+        'base_url' => env('OPENROUTER_BASE_URL', 'https://openrouter.ai/api/v1'),
+        'api_key' => env('OPENROUTER_API_KEY'),
+    ],
 ];
